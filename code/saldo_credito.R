1a1e53cd1eb554004758d2805ebcb928993ebc7e
Francisco Junior <fjunior.alves.oliveira@gmail.com>
2017-12-19 19:56:50 -0200

    Atualiza bases de dados

--- code/saldo_credito.R
+++ code/saldo_credito.R
@@ -1,10 +1,13 @@
-library(relatorios)
+library(relatorios); library(reest)
 library(data.table)
 
 exec_supl <- ler_exec_desp("data-raw/exec_suplementacao.XLS")
 exec_desp <- ler_exec_desp("data-raw/exec_desp.XLS")
+exec_cota <- ler_exec_desp("data-raw/exec_cota.XLS")
+reest_desp <- ler_reest_desp("data-raw/reest_desp.xlsx")
 
-base <- join(exec_supl, exec_desp)
+
+base <- join(SUPL = exec_supl, DESP = exec_desp, REEST = reest_desp, COTA = exec_cota)
 
 base[, ASPS := is_asps_desp(base, "ACAO_COD")]
 base[, MDE := is_mde_desp(base)]
